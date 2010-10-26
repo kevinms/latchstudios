68584b5a31add6a84984f0191b883bcb730388ba
drago3711 <drago3711@a3c018c5-9cca-205f-64a8-5d73662e8435>
2010-10-26 16:05:40 +0000

    blank peer box

---
+++ trunk/peer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import pygame
+
+def main():
+	windowSize = 640,480
+	pygame.init()
+	screen = pygame.display.set_mode(windowSize,0,8)
+	pygame.display.set_caption('Made by: Latch Studios')
+
+
+
+if __name__ == "__main__":
+	main()
