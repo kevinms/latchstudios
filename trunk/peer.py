1511be7c1773e39ef85fade2890ad2a1b32ad34a
drago3711 <drago3711@a3c018c5-9cca-205f-64a8-5d73662e8435>
2010-10-26 19:44:00 +0000

    Created main menu and demo settings menu

--- trunk/peer.py
+++ trunk/peer.py
@@ -13,9 +13,21 @@ def main():
 	screen = pygame.display.set_mode(windowSize,0,8)
 	pygame.display.set_caption('Made by: Latch Studios')
 	screen.fill((159, 180,200))
-	mainMenu(screen)
+	action = mainMenu(screen)
+	if action == 0 :
+		newGame()
+	elif action == 1:
+		connectGame()
+	elif action == 2:
+		settings()
+	else:
+		print "Exiting"
+		exit()
+	screen.fill((159, 180,200))
+
+
+
 	pygame.display.update()
-	
 	done = False
 	while not done:
 		#event handling
@@ -29,11 +41,20 @@ def main():
 
 	print "Exiting"
 
+def newGame():
+	print "Starting New Game"
+
+def connectGame():
+	print "Connecting To Game"
+
+def settin():
+	print "Opening Settings"
 
 
 def mainMenu(screen):
 	
-	font = pygame.font.Font(None, 30)
+	selec = 0
+	font = pygame.font.Font(None, 60)
 	selected = (255,255, 0)
 	unselected = (255,255, 255)
 	newGame = font.render('Create Game', True, selected, (159, 182, 205))
@@ -47,16 +68,16 @@ def mainMenu(screen):
 	quitR = quit.get_rect()
 
 	newGameR.centerx = screen.get_rect().centerx 
-	newGameR.centery = screen.get_rect().centery - 30
+	newGameR.centery = screen.get_rect().centery - 60
 
 	joinGameR.centerx = screen.get_rect().centerx
 	joinGameR.centery = screen.get_rect().centery
 
 	settingsR.centerx = screen.get_rect().centerx
-	settingsR.centery = screen.get_rect().centery + 30
+	settingsR.centery = screen.get_rect().centery + 60
 
 	quitR.centerx = screen.get_rect().centerx
-	quitR.centery = screen.get_rect().centery + 60
+	quitR.centery = screen.get_rect().centery + 120
 
 	screen.blit(newGame,newGameR)
 	screen.blit(joinGame,joinGameR)
@@ -70,11 +91,155 @@ def mainMenu(screen):
 	while not done:
 		#event handling
 		events = pygame.event.get()
+		tmp = selec
 		for e in events:
 			if(e.type == QUIT):
 				print "Exiting"
 				exit()
-			else:
-				pass
+			elif(e.type == pygame.KEYDOWN):
+				if (e.key == K_UP):
+					if selec == 0:
+						selec = 3
+					else:
+						selec = selec - 1
+				elif (e.key == K_DOWN):
+					if selec == 3:
+						selec = 0
+					else:
+						selec = selec + 1
+				elif (e.key == K_RETURN):
+					if selec == 2:
+						print "settings"
+						settin(screen)
+						screen.fill((159, 180,200))	
+						screen.blit(newGame,newGameR)	
+						screen.blit(joinGame,joinGameR)		
+						screen.blit(settings,settingsR)	
+						screen.blit(quit,quitR)
+					else:
+						return selec
+				if tmp == 0:
+					newGame = font.render('Create Game', True, unselected, (159, 182, 205))
+					screen.blit(newGame,newGameR)
+				elif tmp == 1:
+					joinGame = font.render('Join Game', True, unselected, (159, 182, 205))
+					screen.blit(joinGame,joinGameR)
+				elif tmp == 2:
+					settings = font.render('Settings', True, unselected, (159, 182, 205))
+					screen.blit(settings,settingsR)
+				elif tmp == 3:
+					quit = font.render('Quit', True, unselected, (159, 182, 205))
+					screen.blit(quit,quitR)
+	
+				if selec == 0:
+					newGame = font.render('Create Game', True, selected, (159, 182, 205))
+					screen.blit(newGame,newGameR)
+				elif selec == 1:
+					joinGame = font.render('Join Game', True, selected, (159, 182, 205))
+					screen.blit(joinGame,joinGameR)
+				elif selec == 2:
+					settings = font.render('Settings', True, selected, (159, 182, 205))
+					screen.blit(settings,settingsR)
+				elif selec == 3:
+					quit = font.render('Quit', True, selected, (159, 182, 205))
+					screen.blit(quit,quitR)
+				pygame.display.update()	
+
+def settin(screen):
+	
+	selec = 0
+	font = pygame.font.Font(None, 60)
+	selected = (255,255, 0)
+	unselected = (255,255, 255)
+	menu1 = 'Setting 1'
+	menu2 = 'Setting 2'
+	menu3 = 'Setting 3'
+	menu4 = 'Back'
+	newGame = font.render(menu1, True, selected, (159, 182, 205))
+	joinGame = font.render(menu2, True, unselected, (159, 182, 205))
+	settings = font.render(menu3, True, unselected, (159, 182, 205))
+	quit = font.render(menu4, True, unselected, (159, 182, 205))
+
+	screen.fill((159, 180,200))
+
+	newGameR = newGame.get_rect()
+	joinGameR = joinGame.get_rect()
+	settingsR = settings.get_rect()
+	quitR = quit.get_rect()
+
+	newGameR.centerx = screen.get_rect().centerx 
+	newGameR.centery = screen.get_rect().centery - 60
+
+	joinGameR.centerx = screen.get_rect().centerx
+	joinGameR.centery = screen.get_rect().centery
+
+	settingsR.centerx = screen.get_rect().centerx
+	settingsR.centery = screen.get_rect().centery + 60
+
+	quitR.centerx = screen.get_rect().centerx
+	quitR.centery = screen.get_rect().centery + 120
+
+	screen.blit(newGame,newGameR)
+	screen.blit(joinGame,joinGameR)
+	screen.blit(settings,settingsR)
+	screen.blit(quit,quitR)
+
+
+	pygame.display.update()
+
+	done = False
+	while not done:
+		#event handling
+		events = pygame.event.get()
+		tmp = selec
+		for e in events:
+			if(e.type == QUIT):
+				print "Exiting"
+				exit()
+			elif(e.type == pygame.KEYDOWN):
+				if (e.key == K_UP):
+					if selec == 0:
+						selec = 3
+					else:
+						selec = selec - 1
+				elif (e.key == K_DOWN):
+					if selec == 3:
+						selec = 0
+					else:
+						selec = selec + 1
+				elif (e.key == K_RETURN):
+					if selec == 2:
+						settin()
+						print "settings"
+					else:
+						return selec
+				if tmp == 0:
+					newGame = font.render(menu1, True, unselected, (159, 182, 205))
+					screen.blit(newGame,newGameR)
+				elif tmp == 1:
+					joinGame = font.render(menu2, True, unselected, (159, 182, 205))
+					screen.blit(joinGame,joinGameR)
+				elif tmp == 2:
+					settings = font.render(menu3, True, unselected, (159, 182, 205))
+					screen.blit(settings,settingsR)
+				elif tmp == 3:
+					quit = font.render(menu4, True, unselected, (159, 182, 205))
+					screen.blit(quit,quitR)
+	
+				if selec == 0:
+					newGame = font.render(menu1, True, selected, (159, 182, 205))
+					screen.blit(newGame,newGameR)
+				elif selec == 1:
+					joinGame = font.render(menu2, True, selected, (159, 182, 205))
+					screen.blit(joinGame,joinGameR)
+				elif selec == 2:
+					settings = font.render(menu3, True, selected, (159, 182, 205))
+					screen.blit(settings,settingsR)
+				elif selec == 3:
+					quit = font.render(menu4, True, selected, (159, 182, 205))
+					screen.blit(quit,quitR)
+				pygame.display.update()	
+
+
 if __name__ == "__main__":
 	main()
