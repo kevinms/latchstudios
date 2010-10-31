bf41334e6e5dfb303c644f4af19ffc048c1e9771
kevin4 <kevin4@g.clemson.edu>
2010-10-31 05:07:21 +0000

    Basic message passing implemented

--- net-test.py
+++ net-test.py
@@ -1,5 +1,10 @@
 import net as N
 import sys
+import time
+
+# Run server: python net-test.py 0
+# Run client: python net-test.py 1
+# You can type chat messages into the client
 
 if __name__ == '__main__':
 
@@ -8,8 +13,13 @@ if __name__ == '__main__':
 	# Client
 	if t:
 		n = N.client_thread("localhost",8888)
-		n.start()
-		n.name("pughar")
+		n.connect()
+		#time.sleep(2)
+		#n.name("pughar")
+		while 1:
+			s = sys.stdin.readline()
+			n.chat(s.strip())
+	# Server
 	else:
-		n = N.server_thread(8888)
+		n = N.server_thread(8888,2)
 		n.start()
--- net.py
+++ net.py
@@ -2,8 +2,51 @@ import threading
 import socket
 import select
 import errno
+import struct
+import time
 
-class client_thread(threading.Thread):
+# Handles packing and unpacking data for network packets
+class packet():
+	def pack_input(self,s):
+		pass
+	def unpack_input(self,s):
+		pass
+
+	def unpack_disconnect(self,s):
+		pass
+
+	def pack_ping(self):
+		return struct.pack(">c",chr(0));
+	def unpack_ping(self,s):
+		print 'ping'
+
+	def pack_chat(self,data):
+		return struct.pack(">c"+"h"+str(len(data))+"s",chr(3),len(data),data)
+	def unpack_chat(s):
+		size = struct.unpack(">h",s.recv(2))[0]
+		data = s.recv(size)
+		tmp = struct.unpack(str(size)+"s",data)[0]
+		print tmp
+		return tmp
+
+	def pack_error(self,data):
+		return struct.pack(">c"+"h"+str(len(data))+"s",chr(4),len(data),data)
+	def unpack_error(self,s):
+		size = struct.unpack(">h",s.recv(2))[0]
+		data = s.recv(size)
+		tmp = struct.unpack(str(size)+"s",data)[0]
+		print tmp
+		return tmp
+
+	unpack_map = {
+		0 : unpack_ping,
+		1 : unpack_disconnect,
+		2 : unpack_input,
+		3 : unpack_chat,
+		4 : unpack_error
+	}
+
+class client_thread(threading.Thread,packet):
 	lock = threading.Lock()
 	peerid = 0;
 	name = ''
@@ -14,35 +57,49 @@ class client_thread(threading.Thread):
 		self.host = host
 		self.port = port
 		self.s = socket.socket()
+		self.s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 		
 	def connect(self):
-		print 'hello world'
-		
 		# Connect to the server
-		self.s.connect((self.host,self.port))
-		self.connected = True;
-	
+		try:
+			self.s.connect((self.host,self.port))
+			self.connected = True;
+			print 'hello world'
+		except socket.error, e:
+			print 'could not connect to server'
+
 	def name(self,name):
 		if self.connected:
 			self.name = name
 			self.s.send(self.name)
 
+	def chat(self,data):
+		if self.connected:
+			self.s.send(self.pack_chat(data))
+
 	def synch_state(self):
 		print "LOL, riiiiight....."
 
+	def ping(self):
+		if self.connected:
+			self.s.send(self.pack_ping())
+
 	def run(self):
 		self.connect()
+		while 1:
+			pass
 
-class server_thread(threading.Thread):
+class server_thread(threading.Thread,packet):
 	client_list=[]
 	lock = threading.Lock()
 
-	def __init__(self,port):
+	def __init__(self,port,slots):
 		threading.Thread.__init__(self)
 		self.port = port
+		self.slots = slots
 
 	def run(self):
-		self.l = listen_thread(self.port,self.client_list,self.lock)
+		self.l = listen_thread(self.port,self.slots,self.client_list,self.lock)
 		self.l.start()
 		
 		while True:
@@ -53,7 +110,8 @@ class server_thread(threading.Thread):
 	def parse(self,s):
 		data = ""
 		try:
-			data=s.recv(1024)
+			# Try to recieve the type byte
+			data=s.recv(1)
 		except socket.error, e:
 			print "Error, hehehe, but it won't matter now"
 			return
@@ -65,6 +123,13 @@ class server_thread(threading.Thread):
 			self.client_list.remove(s)
 			self.lock.release()
 			s.close()
+
+		else:
+			# O(1) type lookup
+			#TODO: need to add a try statement incase there is no map entry aka bad type
+			i = ord(data)
+			self.unpack_map[i](s)
+"""
 		else:
 			print data.strip()
 			
@@ -78,16 +143,19 @@ class server_thread(threading.Thread):
 						self.client_list.remove(client)
 						client.close()
 			self.lock.release()
+"""
 
 # Thread to accept TCP connections from any client
-class listen_thread(threading.Thread):
+class listen_thread(threading.Thread,packet):
 	host = ''
-	def __init__(self,port,client_list,lock):
+	def __init__(self,port,slots,client_list,lock):
 		threading.Thread.__init__(self)
 		self.port = port
+		self.slots = slots
 		self.client_list = client_list
 		self.lock = lock
 		self.s = socket.socket()
+		self.s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 
 	def run(self):
 		self.s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
@@ -96,7 +164,11 @@ class listen_thread(threading.Thread):
 
 		while True:
 			conn, addr = self.s.accept()
-			print addr
-			self.lock.acquire()
-			self.client_list.append(conn)
-			self.lock.release()
+			conn.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+			if len(self.client_list) < self.slots:
+				print addr
+				self.lock.acquire()
+				self.client_list.append(conn)
+				self.lock.release()
+			else:
+				conn.send(self.pack_error("server full"))
