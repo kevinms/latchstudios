3d3816475f33213a29c6a34eb4a8414512f97f50
burningbman <burningbman@a3c018c5-9cca-205f-64a8-5d73662e8435>
2010-11-16 02:16:20 +0000

    added vec.py

---
+++ trunk/vec.py
@@ -0,0 +1,21 @@
+from math import *
+
+def add(x1, y1, x2, y2):
+    x = x1 + x2
+    y = y1 + y2
+    return x, y
+
+def subtract(x1, y1, x2, y2):
+    x = x1 - x2
+    y = y1 - y2
+    return x, y
+
+def length(x, y):
+    return float(sqrt(x * x + y * y))
+
+def unitdir(x1, y1, x2, y2):
+    x, y = subtract(x1, y1, x2, y2)
+    leng = length(x, y)
+    x = float(x / leng)
+    y = float(y / leng)
+    return x, y
