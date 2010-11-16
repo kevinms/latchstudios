a5e349c551c25347249d27a3caeba582d25ca943
burnstheman <burnstheman@a3c018c5-9cca-205f-64a8-5d73662e8435>
2010-11-16 04:36:21 +0000

    unit updates

--- unitTester.py
+++ unitTester.py
@@ -1,7 +1,23 @@
 import support
 
+import speedster
+
 supportUnit = support.Support(15,25)
 
-print supportUnit.speed
+speedy = speedster.Speedster(80,150)
 
-print supportUnit.position()
+
+print speedy.selected
+
+speedy.selected = True
+
+print speedy.selected
+
+print speedy.speed
+
+speedy.speed = 100
+
+print speedy.speed
+
+
+print speedy.position()
