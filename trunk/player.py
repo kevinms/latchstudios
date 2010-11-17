91d1b79e3c2a6a17f97c4f2cf2024c9d98accf30
drago3711 <drago3711@a3c018c5-9cca-205f-64a8-5d73662e8435>
2010-11-17 01:55:06 +0000

    player

---
+++ trunk/player.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import troop
+import speedster
+import building
+
+class Player:
+	troops = []
+	buidings = []
+	mouseX = 50
+	mouseY = 50
+	playerID = -1
+
+	def __init__(self, playerName):
+		self.troops.append(speedster.Speedster(50,50))
+		self.troops.append(troop.Troop(100,100))
+		self.playerID = playerName
+
