0426ab21aa16e2f06c35af9604e92337400fffc7
kevin4 <kevin4@g.clemson.edu>
2010-11-18 18:41:51 +0000

    added some profiler notes and display script

---
+++ profile-stats.py
@@ -0,0 +1,17 @@
+import pstats
+import sys
+
+p = pstats.Stats(sys.argv[1])
+
+# General Stats
+#print "::General Stats::"
+#p.print_stats()
+
+# Cumulative Stats
+print "::Cumulative::"
+p.sort_stats('cumulative').print_stats(10)
+
+# Time consumption:
+# sort according to time spent within each function, and then print the statistics for the top ten functions.
+print "::Time Consumption::"
+p.sort_stats('time').print_stats(10)
