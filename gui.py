b465a8cffdbcface37c68f9602dce44bf09db443
burningbman <burningbman@a3c018c5-9cca-205f-64a8-5d73662e8435>
2010-11-23 03:06:25 +0000

    added self to gui so it would actually work.

--- gui.py
+++ gui.py
@@ -12,13 +12,12 @@ class Gui():
         self.barracks.set_clip((0,0), (90,90))
         self.baseRect = (530,165,90,90)
         self.barracksRect = (530,310,90,90)
+        self.font = pygame.font.Font(pygame.font.match_font('Arial'), 14)
 
     def refresh(self, screen):
     	pygame.display.update()
 
     def drawRightPanel_Player(self, currentPlayer):
-        font = pygame.font.Font(pygame.font.match_font('Arial'), 14)
-        ifont = pygame.font.Font(pygame.font.match_font('Courier_New'), 10)
         gameWindow = pygame.display.get_surface()
     
         selectedUnitType = -1
@@ -60,12 +59,12 @@ class Gui():
             healthBar = gameWindow.subsurface(530,165,100,15)
             healthBar.fill((0,255,0), pygame.Rect(0,0,curHealth,15))
             healthBar.fill((255,0,0), pygame.Rect(maxHealth-curHealth,0,maxHealth-curHealth,15))
-            gameWindow.blit(font.render(str(curHealth) + " / " + str(maxHealth), 0, (0,0,0)), (555,165))
+            gameWindow.blit(self.font.render(str(curHealth) + " / " + str(maxHealth), 0, (0,0,0)), (555,165))
     
             # UNIT INFO #
-            gameWindow.blit(font.render(str(selectedUnit.speed),0, (255,255,255)), (605, 212))
-            gameWindow.blit(font.render(str(selectedUnit.attackDamage),0, (255,255,255)), (605, 235))
-            gameWindow.blit(font.render(str(selectedUnit.attackRange),0, (255,255,255)), (605, 258))
+            gameWindow.blit(self.font.render(str(selectedUnit.speed),0, (255,255,255)), (605, 212))
+            gameWindow.blit(self.font.render(str(selectedUnit.attackDamage),0, (255,255,255)), (605, 235))
+            gameWindow.blit(self.font.render(str(selectedUnit.attackRange),0, (255,255,255)), (605, 258))
     
         # -- BUILDING -- #
         elif (selectedUnitType == 1):
@@ -80,23 +79,19 @@ class Gui():
     '''
 
     def drawTopPanel_Player(self, currentPlayer):
-        font = pygame.font.Font(pygame.font.match_font('Arial'), 14)
-        gameWindow = pygame.display.get_surface()
-    
-        font = pygame.font.Font(pygame.font.match_font('Arial'), 14)
         gameWindow = pygame.display.get_surface()
     
         gameWindow.blit(self.topPanel,(0,0))
     
         # TOP PANEL INFO BAR #
-        gameWindow.blit(font.render('<Game Name Here>', 0, (255,255,255)), (15, 10))
-        gameWindow.blit(font.render(currentPlayer.name, 0, (255,255,255)), (200, 10))
+        gameWindow.blit(self.font.render('<Game Name Here>', 0, (255,255,255)), (15, 10))
+        gameWindow.blit(self.font.render(currentPlayer.name, 0, (255,255,255)), (200, 10))
         if (len(currentPlayer.troops) <= 10):
-            gameWindow.blit(font.render(str(len(currentPlayer.troops))+'/10', 0, (0,255,0)), (313,18))
+            gameWindow.blit(self.font.render(str(len(currentPlayer.troops))+'/10', 0, (0,255,0)), (313,18))
         else:
-            gameWindow.blit(font.render(str(len(currentPlayer.troops))+'/10', 0, (255,0,0)), (313,18))
-        gameWindow.blit(font.render('0/200', 0, (0,255,0)), (385, 18))
-        gameWindow.blit(font.render('$0', 0, (0,255,0)), (469, 18))
+            gameWindow.blit(self.font.render(str(len(currentPlayer.troops))+'/10', 0, (255,0,0)), (313,18))
+        gameWindow.blit(self.font.render('0/200', 0, (0,255,0)), (385, 18))
+        gameWindow.blit(self.font.render('$0', 0, (0,255,0)), (469, 18))
     
     def drawPanels_Player(self, currentPlayer):
         self.drawTopPanel_Player(currentPlayer)
