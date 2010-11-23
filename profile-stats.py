168ffdd4a66ddce708334369fd1bd07f0eabb386
pughar <pughar@a3c018c5-9cca-205f-64a8-5d73662e8435>
2010-11-23 15:56:08 +0000

    fixed deselecting building bug

--- profile-stats.py
+++ profile-stats.py
@@ -15,3 +15,5 @@ p.sort_stats('cumulative').print_stats(10)
 # sort according to time spent within each function, and then print the statistics for the top ten functions.
 print "::Time Consumption::"
 p.sort_stats('time').print_stats(10)
+#p.sort_stats('time').print_callees(.9, 'init')
+#p.sort_stats('time').print_callers(.1, 'init')
