bedd5acc11768178dade410b4643a52c22f116b7
drago3711 <drago3711@a3c018c5-9cca-205f-64a8-5d73662e8435>
2010-11-23 20:20:28 +0000

    missiles added

--- missile.py
+++ missile.py
@@ -1,10 +1,8 @@
-
 import pygame
 import unit
 import math
-import missile
 
-class Troop(unit.Unit):
+class Missile(unit.Unit):
 	def __init__(self, positionX, positionY, colorVal):
 		unit.Unit.__init__(self, positionX, positionY)
 		
@@ -13,26 +11,15 @@ class Troop(unit.Unit):
 
 		self.mySprite = self.normalSprite
 		self.baseSprite = self.mySprite
-		self.unitType = 1
 		self.rotation = 0
 		self.speed = .5
 
-
-		self.bulletList = []
-		for bullets in range(10):
-			self.bulletList.append(missile.Missile(-10, -10, colorVal)
+		self.isActive = False
 		
 		#Offensive
-		self.attackRate = 0.8
-		self.attackRange = 45
 		self.attackDamage = 5
-		self.attackExplosionSize = 15
-		self.attackChanceOfCriticalHit = 0.05
 
 		self.attackAtX = -1
 		self.attackAtY = -1
 
-		#Defensive
-		self.damageResistance = 10
 		self.setupColors(colorVal)
-		self.rotation = 0
