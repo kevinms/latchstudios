dfa9dcb5934c2bd1d661708ffe25286de98a0832
burnstheman <burnstheman@a3c018c5-9cca-205f-64a8-5d73662e8435>
2010-12-09 09:08:06 +0000

    spawnerBot added

---
+++ spawnerBot.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import pygame
+import string
+import net
+import gui
+import player
+import time
+import random
+from math import *
+
+import troop
+import speedster
+import building
+
+import vec
+import netserver as NS
+import netclient as NC
+from pygame.locals import *
+from pygame.color import THECOLORS
+
+
+def main():
+	#connect to
+	print sys.argv[1]
+	#via this port
+	print sys.argv[2]
+	#wait before moveing around
+	print sys.argv[3]
+
+	playerList = []
+
+	n = NC.client_thread(sys.argv[1],int(sys.argv[2]))
+	n.connect()
+
+	time.sleep(int(sys.argv[3]))
+
+	cashRate = 50
+	cashTicks = 0
+	cash = 100
+	DONE = False
+
+	while not DONE:
+		n.send()
+		#Bot Logic Goes Here
+		
+		n.minput(25, random.randint(0,1000), random.randint(0,1000))
+			 
+		#/Bot Logic
+		
+		#Game Engine Logic Is Here
+		n.recv()
+		while not n.recv_queue.empty():
+			tempData = n.recv_queue.get()
+
+if __name__ == "__main__":
+	main()
