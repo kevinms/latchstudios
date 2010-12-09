b064a317d9b3b3f1e19e452ba68216564c3566cf
burningbman <burningbman@a3c018c5-9cca-205f-64a8-5d73662e8435>
2010-12-09 03:44:53 +0000

    added speedster and troop rectangles. Still have errors on gui.py.

--- trunk/gui.py
+++ trunk/gui.py
@@ -2,10 +2,10 @@ import pygame
    
 class Gui:
     def __init__(self):
-        self.base = pygame.image.load('images/base.png').convert()
-        self.barracks = pygame.image.load('images/barracks.png').convert()
+        self.base = pygame.image.load('images/IntranetExploder.png').convert()
+        self.barracks = pygame.image.load('images/Fireblox.png').convert()
         self.defaultRight = pygame.image.load('images/defaultrightpanel.png').convert()
-        self.speedsterRight = pygame.image.load('images/speedster.png').convert()
+        self.speedsterRight = pygame.image.load('images/speedsterPanel.png').convert()
         self.troopRight = pygame.image.load('images/troop.png').convert()
         self.topPanel = pygame.image.load('images/toppanel.png').convert()
         #self.base.set_clip((0,0), (90,90))
@@ -16,8 +16,10 @@ class Gui:
         self.barracksRect = pygame.Rect(530,310,90,90)
         self.font = pygame.font.Font(pygame.font.match_font('Arial'), 14)
         self.selectedUnitType = -1
-        self.speedster = pygame.image.load('images/sprite4.png').convert()
-        self.troop = pygame.image.load('images/arrow.png').convert()
+        self.speedster = pygame.image.load('images/speedster_s.png').convert()
+        self.troop = pygame.image.load('images/troop_s.png').convert()
+        self.troopRect = pygame.Rect(530,300,30,25)
+        self.speedsterRect = pygame.Rect(580,300,30,25)
 
     def refresh(self, screen):
     	pygame.display.update()
@@ -47,16 +49,16 @@ class Gui:
     
         # -- UNIT -- #
         elif (self.selectedUnitType == 0):
-    
+            #gameWindow.blit()
             # TROOP #
             if (selectedUnit.unitType == 1):
                 gameWindow.blit(self.troopRight, (515, 36))
-                gameWindow.blit(self.troop, (570, 90))
+                gameWindow.blit(self.troop, (570, 95))
     
     		# SPEEDSTER #
             elif (selectedUnit.unitType == 4):
-                gameWindow.blit(self.speedsterRight, (515, 36))
-                gameWindow.blit(self.speedster, (570, 90))
+                gameWindow.blit(self.troopRight, (515, 36))
+                gameWindow.blit(self.speedster, (570, 95))
     
             # HEALTH #
             curHealth = selectedUnit.currHealth
@@ -90,10 +92,10 @@ class Gui:
             # HEALTH #
             curHealth = selectedUnit.currHealth
             maxHealth = selectedUnit.maxHealth
-            ratio = 100 / maxHealth
+            ratio = float(100 / maxHealth)
             healthBar = gameWindow.subsurface(530,215,100,15)
-            healthBar.fill((255,0,0), pygame.Rect(0,0,curHealth*ratio,15))
-            healthBar.fill((0,255,0), pygame.Rect(curHealth*ratio,0,(maxHealth-curHealth)*ratio,15))            
+            healthBar.fill((255,0,0), pygame.Rect(0,0,int(curHealth*ratio),15))
+            healthBar.fill((0,255,0), pygame.Rect(int(curHealth*ratio),0,int((maxHealth-curHealth)*ratio),15))            
             gameWindow.blit(self.font.render(str(curHealth) + " / " + str(maxHealth), 0, (0,0,0)), (545,215))
     
     '''
@@ -109,8 +111,6 @@ class Gui:
         gameWindow.blit(self.topPanel,(0,0))
     
         # TOP PANEL INFO BAR #
-        gameWindow.blit(self.font.render('<Game Name Here>', 0, (255,255,255)), (15, 10))
-        gameWindow.blit(self.font.render(currentPlayer.name, 0, (255,255,255)), (200, 10))
         if (len(currentPlayer.troops) <= 10):
             gameWindow.blit(self.font.render(str(len(currentPlayer.troops))+'/10', 0, (0,255,0)), (313,18))
         else:
