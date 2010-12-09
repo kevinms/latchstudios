d9d7cc3c41061070e5d8568187976c13a83c8019
drago3711 <drago3711@a3c018c5-9cca-205f-64a8-5d73662e8435>
2010-12-09 06:15:22 +0000

    bug fix idea1

--- bot1.py
+++ bot1.py
@@ -31,67 +31,46 @@ def main():
 	print sys.argv[3]
 
 	playerList = []
-	pygame.init()
-	windowSize = 640,480
-	screen = pygame.display.set_mode(windowSize,0,8)
 
 	n = NC.client_thread(sys.argv[1],int(sys.argv[2]))
 	n.connect()
 
-	for peer in n.peer_list:
-		playerList.append(player.Player(peer[0], peer[1]))
-
 	time.sleep(int(sys.argv[3]))
 
+	cashRate = 50
+	cashTicks = 0
+	cash = 100
 	DONE = False
 
 	while not DONE:
 		n.send()
 
 
+		cashTicks += 1
+		if cashTicks == cashRate:
+			cash += 1
+			cashTicks = 0
+
 		#Bot Logic Goes Here
-		push = random.randint(0,4)
+		push = random.randint(0,100)
 		
-		if (push == 1):
+		if (push < 25):
 			n.minput(1, random.randint(0,1000), random.randint(0,1000))
-		elif (push == 3):
+		elif (push < 50):
 			n.minput(3, random.randint(0,1000), random.randint(0,1000))
+		elif (push == 53):
+			n.minput(24, random.randint(0,1000), random.randint(0,1000))
+			cash -= 8
+		elif (push == 54):
+			n.minput(25, random.randint(0,1000), random.randint(0,1000))
+			cash -= 10
+			 
 		#/Bot Logic
 		
 		#Game Engine Logic Is Here
 		n.recv()
 		while not n.recv_queue.empty():
 			tempData = n.recv_queue.get()
-			if tempData[2] == 7:
-				playerList.append(player.Player(tempData[3], tempData[4]))
-				for person in playerList:
-					if n.info.cid == person.playerID:
-						mySelf = person
-			for person in playerList:					
-				if tempData[0] == person.playerID:
-					if tempData[2] == 2:
-						if tempData[3][0] == 1:
-							for tro in person.troops:
-								tro.setSelectVal(False)
-							for tro in person.troops:
-								dist = vec.subtract(tempData[3][1], tempData[3][2], tro.getLocationX(), tro.getLocationY())
-								if fabs(dist[0])< tro.size and fabs(dist[1]) < tro.size:
-									tro.setSelectVal(True)
-									break
-								else:
-									tro.setSelectVal(False)
-						elif tempData[3][0] == 3:
-							for tro in person.troops:
-								if tro.isSelected():
-									tro.moveToTargetX = tempData[3][1]
-									tro.moveToTargetY = tempData[3][2]
-
-		for person in playerList:
-			for tro in person.troops:
-				unitDirect = vec.unitdir(tro.getMoveToTargetX(), tro.getMoveToTargetY(), tro.getLocationX(), tro.getLocationY(), tro.getSpeed())
-				tro.setRotation(unitDirect)
-				tro.locationX = tro.locationX + (tro.speed * unitDirect[0])
-				tro.locationY = tro.locationY + (tro.speed * unitDirect[1])
 
 if __name__ == "__main__":
 	main()
