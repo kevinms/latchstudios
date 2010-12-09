3ba785b9b5e60b7cd4ee78a7ccbe92819808bd8a
drago3711 <drago3711@a3c018c5-9cca-205f-64a8-5d73662e8435>
2010-12-09 08:33:37 +0000

    stress

---
+++ stresstest.bash
@@ -0,0 +1,7 @@
+python bot1.py 192.168.10.196 8888 5 &
+python bot1.py 192.168.10.196 8888 5 &
+python bot1.py 192.168.10.196 8888 5 &
+python bot1.py 192.168.10.196 8888 5 &
+python bot1.py 192.168.10.196 8888 5 &
+python peer.py &
+
